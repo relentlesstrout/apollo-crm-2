8e60ca3eb0241c6e698a84eae720f98947355fe1
Matthew Worrall <matthew.lloyd.worrall@gmail.com>
2026-04-30 13:34:18 +0100

    log out and profile button added

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/logo.png') }}" alt="Apollo Window Cleaners" {{ $attributes->merge(['class' => 'h-20 w-auto']) }}/>
+<img src="{{ asset('images/logo.png') }}" alt="Apollo Window Cleaners" {{ $attributes->merge(['class' => 'h-15 w-auto']) }}/>
